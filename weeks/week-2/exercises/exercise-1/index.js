dbf3a3d312b8b2eaa1c41584492b3016a441972b
SETI108 <seti108@gmail.com>
2023-10-04 08:15:52 +0200

    Restored changes

--- weeks/week-2/exercises/exercise-1/index.js
+++ weeks/week-2/exercises/exercise-1/index.js
@@ -1,42 +1,73 @@
 // Select the element with the class "box" and store it in the "box" variable.
 const box = document.querySelector(".box");
 
-// Select all input elements with the name attribute set to "theme" and store them in the "themeRadios" variable.
-const themeRadios = document.querySelectorAll('input[name="theme"]');
+// console.log("Start Here 😉");
 
-// Loop through each radio button in the "themeRadios" NodeList.
-themeRadios.forEach((radio) => {
-  // Add an event listener to each radio button to listen for the "change" event.
-  // This event triggers when the radio button's state changes (i.e., when it's selected).
-  radio.addEventListener("change", function () {
-    let color; // Declare a variable to store the background color value.
+// Order: Sebastian, Usha, Pernilla
 
-    // Check the value of the currently selected radio button (using 'this') and set the "color" variable accordingly.
-    if (this.value === "dark") {
-      color = "darkgray";
-    } else if (this.value === "light") {
-      color = "lightblue";
-    } else if (this.value === "crazy") {
-      color = "purple";
-    } else {
-      color = "lightgreen"; // Default color.
-    }
 
-    // Add the "color-transition" class to the "box" element.
-    // This class presumably contains CSS that provides a smooth color transition effect.
-    box.classList.add("color-transition");
+/*
 
-    // Change the background color of the "box" element to the value stored in the "color" variable.
-    box.style.backgroundColor = color;
+Let's use some DOM manipulation to target the div container element with the class of "box" and through 
+some DOM manipulation lets change the background color of this element once the user selects a theme from the theme options.
 
-    // Set a timeout to execute a function after a delay of 300 milliseconds.
-    setTimeout(() => {
-      // After the delay, remove the "color-transition" class from the "box" element.
-      // This might be done to ensure that subsequent changes to the box's style don't have the transition effect.
-      box.classList.remove("color-transition");
-    }, 300); // 300 milliseconds delay.
-  });
+*/
+
+const boxContainer = document.querySelector(".box");
+
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+
+
+
+console.log(radioButtons);
+function changeBackground() {
+    boxContainer.style.backgroundColor = "darkgray";
+}
+function changeTheme() {
+    radioButtons.forEach((radioButton) => {
+        const themeSelected = radioButton.value;
+        console.log(radioButton);
+        if(radioButton.checked) {
+            const backgroundColors = {
+                dark : 'darkgray',
+                light: 'lightgray',
+                crazy: 'magenta'};
+            // check which button is selected
+            boxContainer.style.backgroundColor = themeSelected[backgroundColors];    
+        }
+        
+    })
+}
+
+radioButtons.forEach((radioButton) => {
+    radioButton.addEventListener('change', changeTheme);
+    
+})
+
+// changeTheme();
+/*
+const darkButton = document.getElementById("dark");
+darkButton.addEventListener('click', () => {
+    // when the radio is changed listen to that event and update the color using background function
+    switch(id) {
+        case "dark":
+            boxContainer.style.backgroundColor = "darkgray";
+            break;
+        case "light":
+            boxContainer.style.backgroundColor = "lightgray";
+            break;
+        case "crazy":
+            boxContainer.style.backgroundColor = "magenta";
+            break;
+    default:
+        boxContainer.style.backgroundColor = "green";
+    } 
 });
+
+const formTag = document.getElementsByTagName("form");
+console.log(formTag);
+*/
 
 
 
@@ -93,3 +124,4 @@ themeRadios.forEach((radio) => {
     }, 300); // 300 milliseconds delay.
   });
 });
+ */
