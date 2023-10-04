af6d2762bc7f584ddb8a6213b092b903bff39d18
SETI108 <seti108@gmail.com>
2023-10-04 08:45:06 +0200

    Added my own solution for exercise 1

--- weeks/week-2/exercises/exercise-1/index.js
+++ weeks/week-2/exercises/exercise-1/index.js
@@ -1,3 +1,5 @@
+//TEAM SOLUTION
+/*
 // Select the element with the class "box" and store it in the "box" variable.
 const box = document.querySelector(".box");
 
@@ -6,13 +8,13 @@ const box = document.querySelector(".box");
 // Order: Sebastian, Usha, Pernilla
 
 
-/*
+
 
 Let's use some DOM manipulation to target the div container element with the class of "box" and through 
 some DOM manipulation lets change the background color of this element once the user selects a theme from the theme options.
 
 */
-
+/*
 const boxContainer = document.querySelector(".box");
 
 const radioButtons = document.querySelectorAll('input[type="radio"]');
@@ -46,7 +48,7 @@ radioButtons.forEach((radioButton) => {
 })
 
 // changeTheme();
-/*
+
 const darkButton = document.getElementById("dark");
 darkButton.addEventListener('click', () => {
     // when the radio is changed listen to that event and update the color using background function
@@ -71,18 +73,7 @@ console.log(formTag);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+// TEACHER SOLUTION
 
 /*
 // Select the element with the class "box" and store it in the "box" variable.
@@ -125,3 +116,41 @@ themeRadios.forEach((radio) => {
   });
 });
  */
+
+// MY SOLUTION
+
+// PROBLEM
+
+// Make the background of the div "box" change background color to the corresponding color once the radio button is changed
+
+//PLAN
+
+// Select the box container and the form element
+
+const box = document.querySelector('.box');
+const radioButtons = document.getElementById('form');
+
+// Add a function to change the color using a parameter
+
+function changeThemeColor(color){
+  // Write the code to change the background color where the actual color will be the argument in the function
+  box.style.background = color;
+}
+
+// Set up and event listener that triggers once the radio button is changed, check which theme is selected and create a switch statement that enables the colors with the function
+
+// Listen to the parent element, determine which of the buttons were marked and use an if statement to trigger the changeThemeColor function.
+radioButtons.addEventListener('change', function (e) {
+    let selecteTarget = e.target.value;
+     switch(selecteTarget){
+      case 'light':
+        changeThemeColor('white');
+        break;
+      case 'dark':
+        changeThemeColor('darkgray');
+        break;
+      case 'crazy':
+        changeThemeColor('purple');
+     }
+})
+
