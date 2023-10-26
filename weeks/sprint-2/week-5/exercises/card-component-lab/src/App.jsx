37ccc0d70aa6c7c24d443c439a95e3a598b6ef79
SETI108 <seti108@gmail.com>
2023-10-26 11:34:22 +0200

    Team exercise 2 week 5

--- weeks/sprint-2/week-5/exercises/card-component-lab/src/App.jsx
+++ weeks/sprint-2/week-5/exercises/card-component-lab/src/App.jsx
@@ -2,9 +2,48 @@ import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  const lemonData = {
+    title: "Lemon",
+    titleColor: "#166F39",
+    subtitle: "This is a really tasty fruit.",
+    emoji: "🍋",
+    primaryColor: "#8cffba",
+    secondaryColor: "#cfffe2",
+    buttonColor: "#FCED84",
+    buttonText: "ORDER NOW",
+    buttonTextColor: "#000",
+  };
+
+  const rocketData = {
+    title: "Rocket",
+    titleColor: "#0B0C11",
+    subtitle: "Join us into the outer space!",
+    emoji: "🚀",
+    primaryColor: "#A6CFE2",
+    secondaryColor: "#C2DCE9",
+    buttonColor: "#0B0C11",
+    buttonText: "BOOK SEAT",
+    buttonTextColor: "#FFD600",
+  };
+
+  const dogData = {
+    title: "Doggy",
+    titleColor: "#502F7E",
+    subtitle: "Hello, do you want to be friends?",
+    emoji: "🐶",
+    primaryColor: "#EEB200",
+    secondaryColor: "#FFE086",
+    buttonColor: "#FFF",
+    buttonText: "ADOPT ME",
+    buttonTextColor: "#502F7E",
+  };
   return (
     <>
-      <Card />
+      <main>
+        <Card data={lemonData} />
+        <Card data={rocketData} />
+        <Card data={dogData} />
+      </main>
     </>
   );
 }
--- weeks/sprint-2/week-5/exercises/card-component-lab/src/components/Card.jsx
+++ weeks/sprint-2/week-5/exercises/card-component-lab/src/components/Card.jsx
@@ -1,11 +1,65 @@
+import { useState } from "react";
 import "./Card.css";
 
-const Card = () => {
+const Card = ({ data }) => {
+  console.log(data.color);
+  const [isClicked, setIsClicked] = useState(false);
   // If the card is clicked, render this:
   // return ();
 
-  // If the card isn't cliked, render this:
-  return <div>Card goes here</div>;
-};
+  // When clicking on the button you should set a state and with the help of
+  // conditional rendering show this component instead 👉
+  const handleClick = () => {
+    setIsClicked(true);
+  };
 
+  // If the card isn't cliked, render this:
+
+  return (
+    <>
+      {isClicked && (
+        <div
+          style={{ backgroundColor: data.primaryColor }}
+          className="card is-clicked"
+        >
+          <div
+            style={{ backgroundColor: data.primaryColor }}
+            className="title-part"
+          ></div>
+          <div
+            className="light-part"
+            style={{ backgroundColor: data.secondaryColor }}
+          ></div>
+        </div>
+      )}
+      {!isClicked && (
+        <div style={{ backgroundColor: data.primaryColor }} className="card">
+          <div className="header">
+            <h2 style={{ color: data.titleColor }} className="title">
+              {data.title}
+            </h2>
+            <span>{data.emoji}</span>
+          </div>
+          <div
+            style={{ backgroundColor: data.secondaryColor }}
+            className="light-part"
+          >
+            <h4 className="subtitle">{data.subtitle}</h4>
+            <button
+              style={{ backgroundColor: data.buttonColor }}
+              onClick={handleClick}
+            >
+              <p
+                style={{ color: data.buttonTextColor }}
+                className="button-text"
+              >
+                {data.buttonText}
+              </p>
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 export default Card;
