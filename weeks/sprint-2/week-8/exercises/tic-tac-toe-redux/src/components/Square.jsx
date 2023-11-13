41c9f50fe781c0446f523ed38ddc6028dade0f75
SETI108 <seti108@gmail.com>
2023-11-13 21:05:11 +0100

    Comple exercise

--- weeks/sprint-2/week-8/exercises/tic-tac-toe-redux/src/components/Square.jsx
+++ weeks/sprint-2/week-8/exercises/tic-tac-toe-redux/src/components/Square.jsx
@@ -1,8 +1,16 @@
 import { Player } from "./Player";
+import { game } from "../reducers/game";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-export const Square = ({ value }) => {
+export const Square = ({ value, index }) => {
+  const dispatch = useDispatch();
   const handleClick = () => {
     // TODO send the captureSquare action
+    dispatch(game.actions.captureSquare({ index }));
+    // let squareArray = useSelector((store) => store.squares);
+
+    // console.log(squareArray);
   };
 
   return (
